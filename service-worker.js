d928d011a227055a961e7e838aca7d1cc8f8b68e
cleitinho1234 <cleitinhodacruzsilva4@gmail.com>
2026-03-09 20:20:44 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,27 @@
-self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
+const CACHE_NAME = "planta-facil-v1";
+
+const urlsToCache = [
+"/",
+"/index.html",
+"/manifest.json",
+"/icon-192.png",
+"/icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(function(cache){
+return cache.addAll(urlsToCache);
+})
+);
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", function(event){
+event.respondWith(
+caches.match(event.request)
+.then(function(response){
+return response || fetch(event.request);
+})
+);
 });
